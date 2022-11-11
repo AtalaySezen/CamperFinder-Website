5ed6a8a2552b55b731cf86c23c36bb652d2e4f53
AtalaySezen <atalaysezen@yahoo.com.tr>
2022-11-12 00:49:18 +0300

    update

--- scripts/index.js
+++ scripts/index.js
@@ -2,24 +2,40 @@ let searchInput = document.getElementById('searchInput');
 let searchButton = document.getElementById('submit-search');
 
 
-
 searchButton.addEventListener('click', () => {
-    const fruits = ["Banana", "Orange", "Apple", "Mango"];
-    let index = fruits.indexOf(searchInput.value);
-    console.log(index)//2
-    console.log(placesData);
-    console.log(searchInput.value);
-    GetData();
+
 })
 
 //Fetch Data
-let urlPlaces = "http://localhost:3000/places";
-const placesData = [];
-function GetData() {
-    fetch(urlPlaces)
-        .then(response => response.json())
-        .then(data => {
-            placesData.push(data);
-            
-        })
+const api_url = "http://localhost:3000/places";
+
+async function getapi(url) {
+    // Storing response
+    const response = await fetch(url);
+    var data = await response.json();
+    console.log(data);
+    show(data);
+}
+// Calling that async function
+getapi(api_url);
+
+// Function to define innerHTML for HTML table
+function show(data) {
+    let tab =
+        `<tr>
+          <th>ID</th>
+          <th>City</th>
+          <th>District</th>
+          <th>Camping Place</th>
+         </tr>`;
+    // Loop to access all rows 
+    for (let r of data) {
+        tab += `<tr> 
+    <td>${r.id} </td>
+    <td>${r.city}</td>
+    <td>${r.district}</td> 
+    <td>${r.campPlaceName}</td>          
+</tr>`;
+    }
+    document.getElementById("test").innerHTML = tab;
 }

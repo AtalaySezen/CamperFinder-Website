b35ab230dfb1bf29f42dc44770612315006514e1
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2022-11-15 01:15:32 +0300

    feats : admin-panel angular added

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-//Loader Function
-const myPreloader = document.querySelector('.preloader');
-
-document.addEventListener('DOMContentLoaded',() => {
-    setTimeout(() => {
-        myPreloader.style.display = 'none';
-    }, 100);
-}
-)

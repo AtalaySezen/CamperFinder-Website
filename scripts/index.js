dd22e8f2a31092a47097eba19f15d277c5a65dd5
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2022-11-14 16:50:47 +0300

    feats: homepage datas fetched

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,66 @@
 let searchInput = document.getElementById('searchInput');
-let searchButton = document.getElementById('submit-search');
+let searchButton = document.getElementById('searchButton');
 
 
-searchButton.addEventListener('click', () => {
+//Get Datas
+const apiUrl = 'http://localhost:3000/places';
 
-})
-
-//Fetch Data
-const api_url = "http://localhost:3000/places";
-
-async function getapi(url) {
-    // Storing response
+async function getApi(url){
     const response = await fetch(url);
-    var data = await response.json();
-    console.log(data);
+
+    data = await response.json();
+    console.log(data);    
     show(data);
 }
-// Calling that async function
-getapi(api_url);
 
-// Function to define innerHTML for HTML table
-function show(data) {
-    let tab =
-        `<tr>
-          <th>ID</th>
-          <th>City</th>
-          <th>District</th>
-          <th>Camping Place</th>
-         </tr>`;
-    // Loop to access all rows 
-    for (let r of data) {
-        tab += `<tr> 
-    <td>${r.id} </td>
-    <td>${r.city}</td>
-    <td>${r.district}</td> 
-    <td>${r.campPlaceName}</td>          
-</tr>`;
+getApi(apiUrl);
+
+//Show Data HTML
+function show(data){
+    let html = ``;
+
+    for(let x of data){
+        html += `
+        ${x.city}
+        `;
     }
-    document.getElementById("test").innerHTML = tab;
+    document.getElementById('test').innerHTML = html;
 }
+
+//Show Cards Data: 
+let apiCards = 'http://localhost:3000/cards';
+
+async function cardsApi(url){
+    const response = await fetch(url);
+
+    data = await response.json();
+    console.log(data);    
+    showCards(data);
+}
+function showCards(data){
+    let html = ``;
+
+    for(let x of data){
+   
+        html += `
+        <div class="card ${x.city}" id="card-city">
+        <div id="card-head" class="card-head">
+        <h1 class="header-card">${x.city}</h1>
+        </div>
+        <div id="card-image" class="image">
+        <img class="card-image" src="./assets/images/pexels-bayu-jefri-743765.jpg" alt="Kamp Yerleri Bul">                
+        </div>
+        <div class="card-subheader">
+        <p class="info-p">
+        ${x.cardInfo}
+        </p>
+        <button class="btn-discover" href="javascript:void">Keşfet</button>
+        </div>
+        </div>
+        `;
+    }
+    document.getElementById('card-group').innerHTML = html;
+}
+
+cardsApi(apiCards)
+

2659aa6b8606716c438e9a7e1a184e69302cd221
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2022-11-17 12:41:16 +0300

    changes : detail page and places page design

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -2,47 +2,22 @@ let searchInput = document.getElementById('searchInput');
 let searchButton = document.getElementById('searchButton');
 
 
-//Get Datas
-// const apiUrl = 'http://localhost:3000/places';
-
-// async function getApi(url){
-//     const response = await fetch(url);
-
-//     data = await response.json();
-//     console.log(data);    
-//     show(data);
-// }
-
-// getApi(apiUrl);
-
-// //Show Data HTML
-// function show(data){
-//     let html = ``;
-
-//     for(let x of data){
-//         html += `
-//         ${x.city}
-//         `;
-//     }
-//     document.getElementById('test').innerHTML = html;
-// }
-
 //Show Cards Data: 
 let apiCards = 'http://localhost:3000/cards';
 
 async function cardsApi(url) {
-    const response = await fetch(url);
+  const response = await fetch(url);
 
-    data = await response.json();
-    console.log(data);
-    showCards(data);
+  data = await response.json();
+  console.log(data);
+  showCards(data);
 }
 function showCards(data) {
-    let html = ``;
-    
-    for (let x of data) {
-        html += `
-        <a href="${x.city}.html">
+  let html = ``;
+
+  for (let x of data) {
+    html += `
+        <a  href="${x.href}.html">
         <div class="card ${x.city}" id="card-city">
         <div id="card-head" class="card-head">
         <h1 class="header-card">${x.city}</h1>
@@ -60,8 +35,8 @@ function showCards(data) {
         </a>
 
         `;
-    }
-    document.getElementById('card-group').innerHTML = html;
+  }
+  document.getElementById('card-group').innerHTML = html;
 }
 
 cardsApi(apiCards);
@@ -71,23 +46,76 @@ cardsApi(apiCards);
 document.getElementById('noneSearch').style.display = "none";
 
 function myFunction() {
-    
-    // Declare variables
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('searchInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
-    
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-        document.getElementById('noneSearch').style.display = "flex";
-      a = li[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
-      }
+
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('searchInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    document.getElementById('noneSearch').style.display = "flex";
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    console.log(txtValue, "a")
+    txtValue.replace("I", "İ")
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
     }
   }
+}
+
+
+let seasonsText = document.getElementById('seasons');
+//Text According To Seasons
+function getCurrentSeason() {
+  // It's plus one because January is index 0
+  const now = new Date();
+  const month = now.getMonth() + 1;
+
+  if (month > 3 && month < 6) {
+    seasonsText.innerHTML = "İlkbahar'ın tadını en güzel kamp alanlarında çıkarın.";
+    return 'spring';
+
+  }
+
+  if (month > 6 && month < 9) {
+    seasonsText.innerHTML = "Yaz'ın tadını en güzel kamp alanlarında çıkarın.";
+    return 'summer';
+  }
+
+  if (month > 9 && month < 12) {
+    seasonsText.innerHTML = "Sonbahar'ın tadını en güzel kamp alanlarında çıkarın.";
+    return 'fall';
+  }
+
+  if (month >= 1 && month < 3) {
+    seasonsText.innerHTML = "Kış'ın tadını en güzel kamp alanlarında çıkarın.";
+    return 'winter';
+  }
+
+  const day = now.getDate();
+  if (month === 3) {
+    return day < 22 ? 'winter' : 'spring';
+  }
+
+  if (month === 6) {
+    return day < 22 ? 'spring' : 'summer';
+  }
+
+  if (month === 9) {
+    return day < 22 ? 'summer' : 'fall';
+  }
+
+  if (month === 12) {
+    return day < 22 ? 'fall' : 'winter';
+  }
+
+  console.error('Unable to calculate current season');
+}
+
+getCurrentSeason();
--- frontend/scripts/istanbul.js
+++ frontend/scripts/istanbul.js
@@ -2,36 +2,23 @@ let apiPlaces = "http://localhost:3000/places";
 
 async function placesApi(url) {
   const response = await fetch(url);
-
   data = await response.json();
-  console.log(data);
   showPlaces(data);
 }
+
+//Show Data HTML;
 function showPlaces(data) {
-  let html = ``; 1
-
+  let html = ``; 
   for (let x of data) {
-
-    console.log(x)
     if (x.city == "İstanbul") {
-
       html += `
-      <div class="card" id="card-city" onclick="goDetail(${x.id})">
-      <div id="card-head" class="card-head">
-      <h1 class="header-card">${x.city}</h1>
-      <h3 class="header-card">${x.campPlaceName}</h3>
-      </div>
-      <div id="card-image" class="image">
-      <img class="card-image" src="./assets/images/homepage-bg.jpg" alt="${x.alt}">                
-      </div>
-      <div class="card-subheader">
-      <p class="info-p">
-      ${x.district}
-      </p>
-      </div>
-      </div>
-      `;
-    }
+      <div class="card-flex" onclick="goDetail(${x.id})">
+      <div class="places-card" style="background-image:url('${x.image}');"></div>
+      <small class="click-detail">Detay İçin Tıkla</small>
+      <h1 class="header-place">${x.campPlaceName}</h1>
+      <p class="info-place">${x.info}</p>
+  </div>
+  `;}
     document.getElementById('istanbul').innerHTML = html;
   }
 }
@@ -40,16 +27,15 @@ function showPlaces(data) {
 placesApi(apiPlaces);
 
 
-
 function goDetail(id) {
   let params = new URLSearchParams(`id=${id}`);
-  params.get('?id'); // 'node'
-  params.get(id); // '2'
+  params.get('?id'); 
+  params.get(id); 
   window.location.href = ('detailplace.html' + "?" + params)
   //1-Detay sayfasına yönlendirilecek 
   //2-Detay sayfasında query`de idyi verecek. 
-  //3-Detay sayfası yüklenirken, idye bakılacak. O id ile get atılacak. (Detay Sayfası Yükleme Fonksiyonudur.)
-
-
+  //3-Detay sayfası yüklenirken, idye bakılacak. O id ile get atılacak. (Detay Sayfası Yükleme Fonksiyonudur
 }
+
+
 
